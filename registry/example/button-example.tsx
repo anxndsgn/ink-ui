8239a38910daaa0e053ec647b3538f88605b6c50
XIN <60823033+anxndsgn@users.noreply.github.com>
2026-04-19 13:28:57 +0800

    fix: update styles for RevertButtonExample to enhance layout

--- registry/example/button-example.tsx
+++ registry/example/button-example.tsx
@@ -60,7 +60,7 @@ export function LoadingButtonExample() {
 
 export function RevertButtonExample() {
   return (
-    <div className="flex flex-wrap items-center gap-4">
+    <div className="flex flex-wrap items-center gap-4 bg-gray-950 dark:bg-gray-50 p-4 rounded w-full h-32 justify-center">
       <Button variant="revert">Revert</Button>
     </div>
   );
