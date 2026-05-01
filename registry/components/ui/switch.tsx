43595658b3e1d06e3b3f63396161db5f101277c7
XIN <anxanxx@gmail.com>
2026-05-01 19:23:15 +0800

    fix(Switch): adjust thumb translation for improved visual alignment

--- registry/components/ui/switch.tsx
+++ registry/components/ui/switch.tsx
@@ -5,7 +5,8 @@ function Switch({ className, ...props }: BaseSwitch.Root.Props) {
   return (
     <BaseSwitch.Root
       className={cn(
-        "group/switch inline-flex h-4.5 w-7.5 shrink-0 items-center rounded-full border-[0.5px] border-input p-px transition-all outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-checked:bg-accent data-unchecked:bg-muted",
+        "group/switch inline-flex h-4.5 w-7.5 shrink-0 items-center rounded-full border-[0.5px] border-input p-px outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-checked:bg-accent data-unchecked:bg-muted",
+        "transition-all duration-200",
         className,
       )}
       data-slot="switch"
@@ -13,7 +14,7 @@ function Switch({ className, ...props }: BaseSwitch.Root.Props) {
     >
       <BaseSwitch.Thumb
         className={cn(
-          "pointer-events-none block size-3 rounded-full bg-white transition-[translate,width] group-active/switch:w-3.5 data-checked:translate-x-3.5 data-checked:group-active/switch:translate-x-2.5 data-unchecked:translate-x-0.5",
+          "pointer-events-none block size-3 rounded-full bg-white transition-[translate,width] group-active/switch:w-3.5 data-checked:translate-x-3.25 data-checked:group-active/switch:translate-x-2.5 data-unchecked:translate-x-0.5",
         )}
         data-slot="switch-thumb"
       />
