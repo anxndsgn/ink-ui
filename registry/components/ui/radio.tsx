7b9046391ce28d088da2c6f39c9b86d209b40161
XIN <anxanxx@gmail.com>
2026-05-01 18:51:43 +0800

    feat(Radio): update styles for improved accessibility and visual consistency

--- registry/components/ui/radio.tsx
+++ registry/components/ui/radio.tsx
@@ -16,14 +16,21 @@ function Radio({ className, ...props }: RadioPrimitive.Root.Props) {
   return (
     <RadioPrimitive.Root
       className={cn(
-        "relative inline-flex size-4 shrink-0 items-center justify-center rounded-full border border-border bg-muted bg-clip-padding outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive/36 focus-visible:aria-invalid:border-destructive/64 focus-visible:aria-invalid:ring-destructive/48 dark:aria-invalid:ring-destructive/24 [:disabled,[data-checked],[aria-invalid]]:shadow-none",
+        "relative inline-flex size-4 shrink-0 items-center justify-center rounded-full border border-input bg-muted outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive/36 focus-visible:aria-invalid:border-destructive/64 focus-visible:aria-invalid:ring-destructive/48 dark:aria-invalid:ring-destructive/24",
+        "data-checked:border-black/10 data-checked:bg-accent",
         className,
       )}
       data-slot="radio"
       {...props}
     >
       <RadioPrimitive.Indicator
-        className="absolute -inset-px flex size-4 items-center justify-center rounded-full before:size-2 before:rounded-full before:bg-primary data-unchecked:hidden"
+        keepMounted
+        className={cn(
+          "absolute top-1/2 right-1/2 flex size-2 translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-accent-foreground ring ring-black/5 data-unchecked:hidden",
+          "transition-all",
+          "data-starting-style:scale-75",
+          "data-ending-style:scale-75",
+        )}
         data-slot="radio-indicator"
       />
     </RadioPrimitive.Root>
